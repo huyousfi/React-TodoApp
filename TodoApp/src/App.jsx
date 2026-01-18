aa21dcd817f7c43ed2b3b599b64d4109601e59c6
huyousfi <huyousfi@gmail.com>
2026-01-18 10:12:47 +0500

    Todo App stage two with conditional randring

--- TodoApp/src/App.jsx
+++ TodoApp/src/App.jsx
@@ -54,17 +54,76 @@ export default App
 
 // ToDo App stage 1
 
-const todoApp = () => {
+// const todoApp = () => {
+//   return (
+//     <>
+//     <h1>To Do App</h1>
+//     <form >
+//       <input type="text" placeholder="Enter the Title" />
+//       <input type="text" placeholder="Enter the Description" />
+//       <button>Add Todo</button>
+//     </form>
+//     </>
+//   )
+// }
+
+// export default todoApp
+
+
+// ================================================================================
+// ------------------------ToDo App stage 2 with conditional rendering and usestate
+//================================================================================
+
+
+function todoApp() {
+const [title, setTitle] = useState("");
+const [description, setDescription] = useState("");
+const [todo, setTodo] = useState([]);
+
+const addTodo = (event) => {
+  event.preventDefault();
+  console.log(title, description);
+
+  todo.push({title, description});
+
+  setTodo([...todo]);
+  
+  setTitle("");
+  setDescription("");
+};
+
   return (
     <>
     <h1>To Do App</h1>
-    <form >
-      <input type="text" placeholder="Enter the Title" />
-      <input type="text" placeholder="Enter the Description" />
+    <form onSubmit={addTodo}>
+      <input
+      type="text" 
+      value={title} 
+      placeholder="Enter the Title" 
+      onChange={(e) => setTitle(e.target.value)} 
+      />
+      <input 
+      type="text" 
+      value={description} 
+      placeholder="Enter the Description" 
+      onChange={(e) => setDescription(e.target.value)} 
+      />
+
       <button>Add Todo</button>
     </form>
+
+    <div>
+      {todo.length > 0 ? todo.map((item, index) => {
+        return <div className= "children" key={index}>
+        <h1>No: {index + 1}</h1>
+        <p>Title: {item.title}</p>
+        <p>Description: {item.description}</p>
+        <button>Edit Todo</button>
+        <button>Delete Todo</button>
+        </div>
+      }) : <h1>No To Do Found</h1>}
+    </div>
     </>
   )
 }
-
-export default App
+export default todoApp
