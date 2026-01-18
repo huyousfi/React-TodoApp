2f4b6f96011138fcef6bd9cc7c9f459645d6452d
huyousfi <huyousfi@gmail.com>
2026-01-18 10:13:18 +0500

    Todo App completed

--- TodoApp/src/App.jsx
+++ TodoApp/src/App.jsx
@@ -75,6 +75,61 @@ export default App
 //================================================================================
 
 
+// function todoApp() {
+// const [title, setTitle] = useState("");
+// const [description, setDescription] = useState("");
+// const [todo, setTodo] = useState([]);
+
+// const addTodo = (event) => {
+//   event.preventDefault();
+//   console.log(title, description);
+
+//   todo.push({title, description});
+
+//   setTodo([...todo]);
+  
+//   setTitle("");
+//   setDescription("");
+// };
+
+//   return (
+//     <>
+//     <h1>To Do App</h1>
+//     <form onSubmit={addTodo}>
+//       <input
+//       type="text" 
+//       value={title} 
+//       placeholder="Enter the Title" 
+//       onChange={(e) => setTitle(e.target.value)} 
+//       />
+//       <input 
+//       type="text" 
+//       value={description} 
+//       placeholder="Enter the Description" 
+//       onChange={(e) => setDescription(e.target.value)} 
+//       />
+
+//       <button>Add Todo</button>
+//     </form>
+
+//     <div>
+//       {todo.length > 0 ? todo.map((item, index) => {
+//         return <div className= "children" key={index}>
+//         <h1>No: {index + 1}</h1>
+//         <p>Title: {item.title}</p>
+//         <p>Description: {item.description}</p>
+//         <button>Edit Todo</button>
+//         <button>Delete Todo</button>
+//         </div>
+//       }) : <h1>No To Do Found</h1>}
+//     </div>
+//     </>
+//   )
+// }
+// export default todoApp
+
+
+
 function todoApp() {
 const [title, setTitle] = useState("");
 const [description, setDescription] = useState("");
@@ -90,6 +145,21 @@ const addTodo = (event) => {
   
   setTitle("");
   setDescription("");
+};
+
+const deleteTodo = (index) => {
+  todo.splice(index, 1);
+  setTodo([...todo]);
+};
+
+const editTodo = (index) => {
+  // console.log("edit todo", index);
+  const title = prompt("Enter a new title");
+  todo[index].title = title;
+
+  const description = prompt("Enter a new description");
+  todo[index].description = description;
+  setTodo([...todo]);
 };
 
   return (
@@ -118,8 +188,8 @@ const addTodo = (event) => {
         <h1>No: {index + 1}</h1>
         <p>Title: {item.title}</p>
         <p>Description: {item.description}</p>
-        <button>Edit Todo</button>
-        <button>Delete Todo</button>
+        <button onClick={() => editTodo(index)}>Edit Todo</button>
+        <button onClick={() => deleteTodo(index)}>Delete Todo</button>
         </div>
       }) : <h1>No To Do Found</h1>}
     </div>
