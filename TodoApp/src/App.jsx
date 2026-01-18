37618ce2f2e08dda95e34a9a35840ff937dec26e
huyousfi <huyousfi@gmail.com>
2026-01-18 10:09:51 +0500

    Counter stage two used usestate

--- TodoApp/src/App.jsx
+++ TodoApp/src/App.jsx
@@ -1,24 +1,49 @@
 import { useState } from 'react'
 
 // counter first stage without usestate
+// const App = () => {
+//   let num = 0
+//   const addCounter = () => {
+//     num += 1
+//     console.log(num);
+//   }
+//   const lessCounter = () => {
+//     num -= 1
+//     console.log(num);
+//   }
+//   return (
+//     <>
+//     <h1> Counter {num}</h1>
+//     <button onClick={addCounter}>+</button>
+//     <button onClick={lessCounter}>-</button>
+//     </>
+//   )
+// }
+
+
+export default App
+
+
+// counter second stage with usestate
 const App = () => {
-  let num = 0
+  
+  const [num , setNum] = useState(0)
   const addCounter = () => {
-    num += 1
-    console.log(num);
+    setNum(num + 1)
+    // console.log(num);
   }
   const lessCounter = () => {
-    num -= 1
-    console.log(num);
+    setNum(num - 1)
+    // console.log(num);
   }
   return (
     <>
     <h1> Counter {num}</h1>
     <button onClick={addCounter}>+</button>
     <button onClick={lessCounter}>-</button>
+    <hr/>
+
     </>
   )
 }
-
 export default App
-
