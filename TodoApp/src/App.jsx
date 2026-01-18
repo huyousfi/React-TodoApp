095ba5f9019fbf64476e932a42e987f52b1cb1dc
huyousfi <huyousfi@gmail.com>
2026-01-18 10:10:59 +0500

    created fist stage of Todo App, input, button

--- TodoApp/src/App.jsx
+++ TodoApp/src/App.jsx
@@ -25,25 +25,46 @@ export default App
 
 
 // counter second stage with usestate
-const App = () => {
+// const App = () => {
   
-  const [num , setNum] = useState(0)
-  const addCounter = () => {
-    setNum(num + 1)
-    // console.log(num);
-  }
-  const lessCounter = () => {
-    setNum(num - 1)
-    // console.log(num);
-  }
+//   const [num , setNum] = useState(0)
+//   const addCounter = () => {
+//     setNum(num + 1)
+//     // console.log(num);
+//   }
+//   const lessCounter = () => {
+//     setNum(num - 1)
+//     // console.log(num);
+//   }
+//   return (
+//     <>
+//     <h1> Counter {num}</h1>
+//     <button onClick={addCounter}>+</button>
+//     <button onClick={lessCounter}>-</button>
+//     <hr/>
+
+//     </>
+//   )
+// }
+
+
+//================================
+// -------------------------------ToDo App 
+//================================
+
+// ToDo App stage 1
+
+const todoApp = () => {
   return (
     <>
-    <h1> Counter {num}</h1>
-    <button onClick={addCounter}>+</button>
-    <button onClick={lessCounter}>-</button>
-    <hr/>
-
+    <h1>To Do App</h1>
+    <form >
+      <input type="text" placeholder="Enter the Title" />
+      <input type="text" placeholder="Enter the Description" />
+      <button>Add Todo</button>
+    </form>
     </>
   )
 }
+
 export default App
