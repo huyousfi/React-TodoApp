2c7d0e3785644163d19e9068f7343c5ecfc9da12
huyousfi <huyousfi@gmail.com>
2026-01-18 10:08:40 +0500

    counter first stage, created button and title

--- TodoApp/src/App.jsx
+++ TodoApp/src/App.jsx
@@ -1,35 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+// counter first stage without usestate
+const App = () => {
+  let num = 0
+  const addCounter = () => {
+    num += 1
+    console.log(num);
+  }
+  const lessCounter = () => {
+    num -= 1
+    console.log(num);
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1> Counter {num}</h1>
+    <button onClick={addCounter}>+</button>
+    <button onClick={lessCounter}>-</button>
     </>
   )
 }
 
 export default App
+
